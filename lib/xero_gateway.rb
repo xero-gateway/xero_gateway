e143d14bb4842a7464364e1ded30109db60840f7
Tim Connor <tlconnor@gmail.com>
2008-12-01 16:36:45 +1300

    fix get_invoices when a modified_since date is passed

--- lib/xero_gateway.rb
+++ lib/xero_gateway.rb
@@ -12,6 +12,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+require 'cgi'
 require "uri"
 require 'net/https'
 require "rexml/document"
