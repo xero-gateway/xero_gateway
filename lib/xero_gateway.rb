3157246cc31c418fec39f640b8cc8c8532ea41b6
Tim Connor <tlconnor@gmail.com>
2008-12-01 15:49:10 +1300

    replace require 'URI' with require 'uri'

--- lib/xero_gateway.rb
+++ lib/xero_gateway.rb
@@ -12,7 +12,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-require "URI"
+require "uri"
 require 'net/https'
 require "rexml/document"
 require "builder"
