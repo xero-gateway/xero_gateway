1cab0e9b7145da31eeefd19b4d8de3c6cc757a38
Tim Connor <tlconnor@gmail.com>
2008-12-03 14:07:32 +1300

    add unit test for account_message

--- lib/xero_gateway/messages/account_message.rb
+++ lib/xero_gateway/messages/account_message.rb
@@ -2,6 +2,18 @@ module XeroGateway
   module Messages
     class AccountMessage      
       
+      def self.build_xml(account)
+        b = Builder::XmlMarkup.new
+        
+        b.Account {
+          b.Code account.code
+          b.Name account.name
+          b.Type account.type
+          b.TaxType account.tax_type
+          b.Description account.description
+        }
+      end
+      
       # Take an Account element and convert it into an Account object
       def self.from_xml(account_element)        
         account = Account.new
@@ -15,30 +27,6 @@ module XeroGateway
           end
         end      
         account
-      end
-      
-      private
-      
-      def self.parse_line_item(line_item_element)
-        line_item = LineItem.new
-        line_item_element.children.each do |element|
-          case(element.name)
-            when "LineItemID" then line_item.line_item_id = element.text
-            when "Description" then line_item.description = element.text
-            when "Quantity" then line_item.quantity = element.text.to_i
-            when "UnitAmount" then line_item.unit_amount = BigDecimal.new(element.text)
-            when "TaxType" then line_item.tax_type = element.text
-            when "TaxAmount" then line_item.tax_amount = BigDecimal.new(element.text)
-            when "LineAmount" then line_item.line_amount = BigDecimal.new(element.text)
-            when "AccountCode" then line_item.account_code = element.text
-            when "Tracking" then
-            if element.elements['TrackingCategory']
-              line_item.tracking_category = element.elements['TrackingCategory/Name'].text
-              line_item.tracking_option = element.elements['TrackingCategory/Option'].text
-            end
-          end
-        end
-        line_item
       end
     end
   end
---
+++ test/unit/messages/account_message_test.rb
@@ -0,0 +1,36 @@
+require File.join(File.dirname(__FILE__), '../../test_helper.rb')
+
+class AccountMessageTest < Test::Unit::TestCase
+  # Tests that an account can be converted into XML that Xero can understand, and then converted back to an account
+  def test_build_and_parse_xml
+    
+    
+    account = create_test_account
+    
+    # Generate the XML message
+    account_as_xml = XeroGateway::Messages::AccountMessage.build_xml(account)
+
+    # Parse the XML message and retrieve the account element
+    account_element = REXML::XPath.first(REXML::Document.new(account_as_xml), "/Account")
+
+    # Build a new account from the XML
+    result_account = XeroGateway::Messages::AccountMessage.from_xml(account_element)
+    
+    # Check the contact details
+    assert_equal account, result_account
+  end
+  
+  
+  private
+  
+  def create_test_account
+    account = XeroGateway::Account.new
+    account.code = "200"
+    account.name = "Sales"
+    account.type = "REVENUE"
+    account.tax_type = "OUTPUT"
+    account.description = "Income from any normal business activity"
+    
+    account
+  end
+end
