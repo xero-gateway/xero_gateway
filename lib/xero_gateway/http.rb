5003743b12e0ebd52832737cd351a0b40a557bbc
Wayne Robinson <wayne.robinson@gmail.com>
2009-08-12 15:42:33 +1000

    Refactor XeroGateawy::HTTP into a single request method with multiple HTTP METHOD aliases for GET/POST/PUT.

--- lib/xero_gateway/http.rb
+++ lib/xero_gateway/http.rb
@@ -4,56 +4,44 @@ module XeroGateway
     READ_TIMEOUT = 60 unless defined? READ_TIMEOUT
 
     def http_get(url, extra_params = {})
-      params = {:apiKey => @api_key, :xeroKey => @customer_key}
-      params = params.merge(extra_params).map {|key,value| "#{key}=#{CGI.escape(value.to_s)}"}.join("&")
-
-      uri   = URI.parse(url + "?" + params)
-
-      http = Net::HTTP.new(uri.host, uri.port) 
-      http.open_timeout = OPEN_TIMEOUT
-      http.read_timeout = READ_TIMEOUT
-      http.use_ssl      = true
-      http.verify_mode    = OpenSSL::SSL::VERIFY_NONE
-
-      http.get(uri.request_uri).body
+      http_request(:get, url, nil, extra_params)
     end
 
     def http_post(url, body, extra_params = {})
-      headers = {}
-      headers['Content-Type'] ||= "application/x-www-form-urlencoded"      
-
-      params = {:apiKey => @api_key, :xeroKey => @customer_key}
-      params = params.merge(extra_params).map {|key,value| "#{key}=#{CGI.escape(value.to_s)}"}.join("&")
-
-      uri   = URI.parse(url + "?" + params)
-
-      http = Net::HTTP.new(uri.host, uri.port) 
-      http.open_timeout = OPEN_TIMEOUT
-      http.read_timeout = READ_TIMEOUT
-      http.use_ssl      = true
-
-      http.verify_mode    = OpenSSL::SSL::VERIFY_NONE
-
-      http.post(uri.request_uri, body, headers).body
+      http_request(:post, url, body, extra_params)
     end
 
     def http_put(url, body, extra_params = {})
-      headers = {}
-      headers['Content-Type'] ||= "application/x-www-form-urlencoded"      
+      http_request(:put, url, body, extra_params)
+    end
+    
+    private
+    
+      def http_request(method, url, body, extra_params = {})
+        headers = {} 
 
-      params = {:apiKey => @api_key, :xeroKey => @customer_key}
-      params = params.merge(extra_params).map {|key,value| "#{key}=#{CGI.escape(value.to_s)}"}.join("&")
+        if method != :get
+         headers['Content-Type'] ||= "application/x-www-form-urlencoded"
+        end
 
-      uri   = URI.parse(url + "?" + params)
+        params = {:apiKey => @api_key, :xeroKey => @customer_key}
+        params = params.merge(extra_params).map {|key,value| "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"}.join("&")
 
-      http = Net::HTTP.new(uri.host, uri.port) 
-      http.open_timeout = OPEN_TIMEOUT
-      http.read_timeout = READ_TIMEOUT
-      http.use_ssl      = true
+        uri   = URI.parse(url + "?" + params)
 
-      http.verify_mode    = OpenSSL::SSL::VERIFY_NONE
+        http = Net::HTTP.new(uri.host, uri.port) 
+        http.open_timeout = OPEN_TIMEOUT
+        http.read_timeout = READ_TIMEOUT
+        http.use_ssl      = true
 
-      http.put(uri.request_uri, body, headers).body    
-    end      
+        http.verify_mode    = OpenSSL::SSL::VERIFY_NONE
+
+        case method
+          when :get   then    http.get(uri.request_uri, headers).body
+          when :post  then    http.post(uri.request_uri, body, headers).body
+          when :put   then    http.put(uri.request_uri, body, headers).body
+        end
+      end
+       
   end
 end
