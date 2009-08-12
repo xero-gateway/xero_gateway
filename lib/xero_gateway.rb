dc09c9e22cb7fcbda5a1620770a3f66622e5e81f
Wayne Robinson <wayne.robinson@gmail.com>
2009-08-12 17:13:30 +1000

    Make xero_gateway.rb requires cross-platform compatible by using File.join (uses File::SEPARATOR).

--- lib/xero_gateway.rb
+++ lib/xero_gateway.rb
@@ -5,16 +5,16 @@ require "rexml/document"
 require "builder"
 require "bigdecimal"
 
-require File.dirname(__FILE__) + "/xero_gateway/http"
-require File.dirname(__FILE__) + "/xero_gateway/dates"
-require File.dirname(__FILE__) + "/xero_gateway/money"
-require File.dirname(__FILE__) + "/xero_gateway/response"
-require File.dirname(__FILE__) + "/xero_gateway/line_item"
-require File.dirname(__FILE__) + "/xero_gateway/invoice"
-require File.dirname(__FILE__) + "/xero_gateway/contact"
-require File.dirname(__FILE__) + "/xero_gateway/address"
-require File.dirname(__FILE__) + "/xero_gateway/phone"
-require File.dirname(__FILE__) + "/xero_gateway/account"
-require File.dirname(__FILE__) + "/xero_gateway/error"
-require File.dirname(__FILE__) + "/xero_gateway/tracking_category"
-require File.dirname(__FILE__) + "/xero_gateway/gateway"
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'http')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'dates')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'money')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'response')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'account')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'tracking_category')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'contact')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'line_item')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'invoice')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'address')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'phone')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'error')
+require File.join(File.dirname(__FILE__), 'xero_gateway', 'gateway')
