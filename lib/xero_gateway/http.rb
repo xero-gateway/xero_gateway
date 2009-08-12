7a7d0a6cbce129aa6bab3a1b863bbe7ec15e1574
Wayne Robinson <wayne.robinson@gmail.com>
2009-08-13 01:58:59 +1000

    Ensure that the CA for Xero's API is validated to prevent man-in-the-middle attacks. Additionally, because the initialisation time is increased with this verification, cache the initialised HTTP object.

--- lib/xero_gateway/http.rb
+++ lib/xero_gateway/http.rb
@@ -2,7 +2,8 @@ module XeroGateway
   module Http
     OPEN_TIMEOUT = 10 unless defined? OPEN_TIMEOUT
     READ_TIMEOUT = 60 unless defined? READ_TIMEOUT
-
+    ROOT_CA_FILE = File.join(File.dirname(__FILE__), 'ca-certificates.crt')
+    
     def http_get(url, extra_params = {})
       http_request(:get, url, nil, extra_params)
     end
@@ -29,17 +30,24 @@ module XeroGateway
 
         uri   = URI.parse(url + "?" + params)
 
-        http = Net::HTTP.new(uri.host, uri.port) 
-        http.open_timeout = OPEN_TIMEOUT
-        http.read_timeout = READ_TIMEOUT
-        http.use_ssl      = true
+        # Only setup @cached_http once on first use as loading the CA file is quite expensive computationally.
+        unless @cached_http && @cached_http.address == uri.host && @cached_http.port == uri.port
+          @cached_http = Net::HTTP.new(uri.host, uri.port) 
+          @cached_http.open_timeout = OPEN_TIMEOUT
+          @cached_http.read_timeout = READ_TIMEOUT
+          @cached_http.use_ssl      = true
 
-        http.verify_mode    = OpenSSL::SSL::VERIFY_NONE
+          # Need to validate server's certificate against root certificate authority to prevent man-in-the-middle attacks.
+          @cached_http.ca_file        = ROOT_CA_FILE
+          # http.verify_mode    = OpenSSL::SSL::VERIFY_NONE
+          @cached_http.verify_mode    = OpenSSL::SSL::VERIFY_PEER
+          @cached_http.verify_depth   = 5
+        end
 
         case method
-          when :get   then    http.get(uri.request_uri, headers).body
-          when :post  then    http.post(uri.request_uri, body, headers).body
-          when :put   then    http.put(uri.request_uri, body, headers).body
+          when :get   then    @cached_http.get(uri.request_uri, headers).body
+          when :post  then    @cached_http.post(uri.request_uri, body, headers).body
+          when :put   then    @cached_http.put(uri.request_uri, body, headers).body
         end
       end
        
