b90586c07d0c23d98b6d05fe38771dc8b22b1079
Nik Wakelin <nik@codetocustomer.com>
2010-04-05 14:13:52 +1200

    Revert OAuth example to a useful version

--- examples/oauth.rb
+++ examples/oauth.rb
@@ -3,25 +3,23 @@ require 'pp'
 
 require File.dirname(__FILE__) + '/../lib/xero_gateway.rb'
 
-XERO_CONSUMER_KEY    = "M2YYZMRJNJHMZDE3NDZKODHJMDAWNZ"
-XERO_CONSUMER_SECRET = "MGY4MMY2MGNIMWY0NGVINMJJMZLLND"
+XERO_CONSUMER_KEY    = "YOUR-OAUTH-CONSUMER-KEY"
+XERO_CONSUMER_SECRET = "YOUR-OAUTH-CONSUMER-SECRET"
 
 gateway = XeroGateway::Gateway.new(XERO_CONSUMER_KEY, XERO_CONSUMER_SECRET)
 
 # authorize in browser
-#{}%x(open #{gateway.request_token.authorize_url})
+%x(open #{gateway.request_token.authorize_url})
 
-#puts "Enter the verification code from Xero?"
-#oauth_verifier = gets.chomp  
+puts "Enter the verification code from Xero?"
+oauth_verifier = gets.chomp  
 
-#gateway.authorize_from_request(gateway.request_token.token, gateway.request_token.secret, :oauth_verifier => oauth_verifier)
+gateway.authorize_from_request(gateway.request_token.token, gateway.request_token.secret, :oauth_verifier => oauth_verifier)
 
-gateway.authorize_from_access("ZMU0MMU4ZWM3MTAWNGM2NJHIZJQZNT", "LFP0MMFAQ0CQSVT4O5CXZI3ALEKHHN")
-
-puts "Your access token: #{gateway.access_token.token} / #{gateway.access_token.secret}"
+puts "Your access token: #{gateway.access_token}"
 puts "(Good for 30 minutes)"
 
 # Example API Call
-#pp gateway.get_contacts.contacts
+pp gateway.get_contacts.contacts
 
-gateway.get_invoice_by_id("bogusid")
+
