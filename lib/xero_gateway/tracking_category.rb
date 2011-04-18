fea020d3715536e9d1d005ba612472f00a7c994b
Tim Connor <tlconnor@gmail.com>
2011-04-18 17:46:15 +1200

    Fix issue with blank tracking category IDs

--- lib/xero_gateway/tracking_category.rb
+++ lib/xero_gateway/tracking_category.rb
@@ -55,7 +55,7 @@ module XeroGateway
     # option, and the Options tag is omitted
     def to_xml_for_invoice_messages(b = Builder::XmlMarkup.new)
       b.TrackingCategory {
-        b.TrackingCategoryID self.tracking_category_id
+        b.TrackingCategoryID self.tracking_category_id unless tracking_category_id.nil?
         b.Name self.name
         b.Option self.options.is_a?(Array) ? self.options.first : self.options.to_s 
       }      
