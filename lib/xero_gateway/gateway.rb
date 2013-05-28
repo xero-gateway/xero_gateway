c982285e5af3b9c912eec67aee1336edd061e911
kemil <kemil07@gmail.com>
2013-05-28 14:40:51 +0700

    updated gateway for story [#49575441]

--- lib/xero_gateway/gateway.rb
+++ lib/xero_gateway/gateway.rb
@@ -194,7 +194,7 @@ module XeroGateway
       
       response_xml = http_get(@client, "#{@xero_url}/Employees", request_params)
 
-      parse_response(response_xml, {:request_params => request_params}, {:request_signature => 'GET/employees'})
+      parse_response(response_xml, {:request_params => request_params}, {:request_signature => 'GET/Employees'})
     end
 
     # Retrieves all invoices from Xero
