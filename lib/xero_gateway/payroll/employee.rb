6c4bc61e37c8a5e97bdeeba23cf4486773343430
kemil <kemil07@gmail.com>
2013-06-04 14:09:18 +0700

    added address field [#49576891]

--- lib/xero_gateway/payroll/employee.rb
+++ lib/xero_gateway/payroll/employee.rb
@@ -16,7 +16,9 @@ module XeroGateway::Payroll
     attr_reader :errors
 
     attr_accessor :employee_id, :first_name, :date_of_birth, :email, :first_name, :gender, :last_name,
-                  :middle_name, :tax_file_number, :title
+                  :middle_name, :tax_file_number, :title,
+                  # Adding HomeAddress fields/elements
+                  :home_address
 
     def initialize(params = {})
       @errors ||= []
@@ -25,6 +27,23 @@ module XeroGateway::Payroll
       params.each do |k,v|
         self.send("#{k}=", v)
       end
+
+      @home_address ||= []
+    end
+
+    # Helper method to add a new address object to this employee.
+    #
+    # Usage:
+    #  employee.add_home_address({
+    #    :address_type =>   'STREET',
+    #    :line_1 =>         '100 Queen Street',
+    #    :city =>           'Brisbane',
+    #    :region =>         'QLD',
+    #    :post_code =>      '4000',
+    #    :country =>        'Australia'
+    #  })
+    def add_home_address(address_params)
+      self.home_address << Address.new(address_params)
     end
 
 
@@ -81,7 +100,10 @@ module XeroGateway::Payroll
         b.LastName self.last_name if self.last_name
         b.MiddleNames self.middle_name if self.middle_name
         b.TaxFileNumber self.tax_file_number if self.tax_file_number
-        b.Title self.title if self.title
+        b.Title self.title if self.title          
+        b.HomeAddress {
+          home_address.each { |home_address| home_address.to_xml(b) }
+        } if self.home_address.any?   
       }
     end
     
@@ -99,13 +121,14 @@ module XeroGateway::Payroll
           when "MiddleNames" then employee.middle_name = element.text
           when "TaxFileNumber" then employee.tax_file_number = element.text
           when "Title" then employee.title = element.text
+          when "HomeAddress" then element.children.each {|home_address_element| employee.home_address << Address.from_xml(home_address_element)}
         end
       end
       employee
     end
 
     def ==(other)
-      [ :employee_id, :first_name, :date_of_birth, :email, :first_name, :gender, :last_name, :middle_name, :tax_file_number, :title ].each do |field|
+      [ :employee_id, :first_name, :date_of_birth, :email, :first_name, :gender, :last_name, :middle_name, :tax_file_number, :title, :home_address ].each do |field|
         return false if send(field) != other.send(field)
       end
       return true
