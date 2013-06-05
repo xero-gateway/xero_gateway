207426ed77be97cd87e4187e73c8cb0e5cec8ea7
Dave Tong <david.mh.tong@gmail.com>
2013-06-05 18:23:00 +1000

    Update payroll example
    [Refs #49576891]

--- examples/payroll_api.rb
+++ examples/payroll_api.rb
@@ -12,9 +12,16 @@ gateway = XeroGateway::Gateway.new(XERO_KEYS["xero_consumer_key"], XERO_KEYS["xe
 %x(open #{gateway.request_token.authorize_url}"&scope=payroll.employees")
 
 puts "Enter the verification code from Xero?"
-oauth_verifier = gets.chomp  
+oauth_verifier = gets.chomp
 
 gateway.authorize_from_request(gateway.request_token.token, gateway.request_token.secret, :oauth_verifier => oauth_verifier)
 
-# Example payroll-API Call
-pp gateway.get_employees.employees.map(&:employee_id)
+# Example payroll-API calls
+payroll_employees = gateway.get_payroll_employees.employees.map(&:employee_id)
+
+pp payroll_employees
+
+# Retrieves Employee details and includes HomeAddress
+payroll_employee = gateway.get_payroll_employee_by_id(payroll_employees.first)
+pp payroll_employee.response_item
+pp payroll_employee.response_item.home_address.address_line1
