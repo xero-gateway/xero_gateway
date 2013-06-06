53756ca8475b68b02c5d1eae300b64a3415a12c7
kemil <kemil07@gmail.com>
2013-06-06 08:04:45 +0700

    added member detail on employee [#49576899]

---
+++ lib/xero_gateway/payroll/bank_account.rb
@@ -0,0 +1,56 @@
+module XeroGateway::Payroll
+  class BankAccount
+    
+    GUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/ unless defined?(GUID_REGEX)
+    
+    attr_accessor :gateway
+    
+    # Any errors that occurred when the #valid? method called.
+    attr_reader :errors
+    
+    attr_accessor :statement_text, :account_name, :bsb, :account_number, :reminder, :percentage, :amount
+    
+     def initialize(params = {})
+      @errors ||= []
+
+      params = {}.merge(params)
+      params.each do |k,v|
+        self.send("#{k}=", v)
+      end
+    end
+    
+    def to_xml(b = Builder::XmlMarkup.new)
+      b.BankAccount {
+      	b.StatementText self.statement_text if self.statement_text
+        b.AccountName self.account_name if self.account_name
+        b.BSB self.bsb if self.bsb
+        b.AccountNumber self.account_number if self.account_number
+        b.Remainder self.reminder if self.reminder
+        b.Percentage self.percentage if self.percentage
+      }
+    end
+    
+    def self.from_xml(bank_account_element, gateway = nil)
+      bank_account = BankAccount.new
+      bank_account_element.children.each do |element|
+        case(element.name)
+          when "StatementText" then bank_account.statement_text = element.text
+          when "AccountName" then  bank_account.account_name = element.text
+          when "BSB" then bank_account.bsb = element.text 
+          when "AccountNumber" then bank_account.account_number = element.text 
+          when "Reminder" then bank_account.reminder = element.text 
+          when "Percentage" then bank_account.percentage = element.text
+        end
+      end
+      bank_account
+    end
+
+    def ==(other)
+      [ :statement_text, :account_name, :bsb, :account_number, :reminder, :percentage, :amount ].each do |field|
+        return false if send(field) != other.send(field)
+      end
+      return true
+    end
+
+  end
+end
---
+++ lib/xero_gateway/payroll/pay_template.rb
@@ -0,0 +1,48 @@
+module XeroGateway::Payroll
+  class PayTemplate
+    
+    GUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/ unless defined?(GUID_REGEX)
+    
+    attr_accessor :gateway
+    
+    # Any errors that occurred when the #valid? method called.
+    attr_reader :errors
+    
+    attr_accessor :earnings_lines
+    
+     def initialize(params = {})
+      @errors ||= []
+
+      params = {}.merge(params)
+      params.each do |k,v|
+        self.send("#{k}=", v)
+      end
+
+      @earnings_lines ||= []
+    end
+    
+    def to_xml(b = Builder::XmlMarkup.new)
+      b.PayTemplate {
+      	b.EarningsLines self.earnings_lines if self.earnings_lines
+      }
+    end
+    
+    def self.from_xml(pay_template_element, gateway = nil)
+      pay_template = PayTemplate.new
+      pay_template_element.children.each do |element|
+        case(element.name)
+          when "EarningsLines" then element.children.each {|child| employee.earnings_lines << EarningsLine.from_xml(child, gateway) }
+        end
+      end
+      pay_template
+    end
+
+    def ==(other)
+      [ :earnings_lines ].each do |field|
+        return false if send(field) != other.send(field)
+      end
+      return true
+    end
+
+  end
+end
