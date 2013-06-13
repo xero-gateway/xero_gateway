84059426638920e75ef8885fca20c775ffb925df
Dave Tong <david.mh.tong@gmail.com>
2013-06-13 15:13:47 +1000

    Update sample
    [refs #49577175]

--- examples/payroll_api.rb
+++ examples/payroll_api.rb
@@ -19,9 +19,21 @@ gateway.authorize_from_request(gateway.request_token.token, gateway.request_toke
 # Example payroll-API calls
 payroll_employees = gateway.get_payroll_employees.employees.map(&:employee_id)
 
+pp "**** get_payroll_employees"
 pp payroll_employees
 
 # Retrieves Employee details and includes HomeAddress
+pp "**** get_payroll_employee_by_id"
 payroll_employee = gateway.get_payroll_employee_by_id(payroll_employees.first)
+pp "**** response_item"
 pp payroll_employee.response_item
+pp "**** home_address"
 pp payroll_employee.response_item.home_address.address_line1
+
+# Retrieves Employee Super details
+pp "**** get_payroll_super_fund_by_id"
+pp gateway.get_payroll_super_fund_by_id(payroll_employee.response_item.super_memberships.first.super_fund_id)
+
+# Retrieves all Super Funds
+pp "**** get_payroll_super_funds"
+pp gateway.get_payroll_super_funds
