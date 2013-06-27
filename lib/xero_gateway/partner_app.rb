f00dd6560ebcbec91f3aaccd1888a33b664db0be
Dominic Wroblewski <domness@gmail.com>
2013-06-27 09:48:20 +0100

    Add expires at to partner_app

--- lib/xero_gateway/partner_app.rb
+++ lib/xero_gateway/partner_app.rb
@@ -6,7 +6,7 @@ module XeroGateway
     NO_SSL_CLIENT_CERT_MESSAGE   = "You need to provide a client ssl certificate and key pair (these are the ones you got from Entrust and should not be password protected) as :ssl_client_cert and :ssl_client_key (should be .crt or .pem files)"
     NO_PRIVATE_KEY_ERROR_MESSAGE = "You need to provide your private key (corresponds to the public key you uploaded at api.xero.com) as :private_key_file (should be .crt or .pem files)"
 
-    def_delegators :client, :session_handle, :renew_access_token, :authorization_expires_at
+    def_delegators :client, :session_handle, :renew_access_token, :authorization_expires_at, :expires_at
 
     def initialize(consumer_key, consumer_secret, options = {})
 
--- xero_gateway.gemspec
+++ xero_gateway.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name     = "xero_gateway"
-  s.version  = "3.0.8"
+  s.version  = "3.0.9"
   s.date     = "2013-06-24"
   s.summary  = "Enables ruby based applications to communicate with the Xero API"
   s.email    = "dave@thinkei.com"
