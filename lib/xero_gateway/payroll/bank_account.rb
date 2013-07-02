7cf3134b231911b978870d91b9b432a2c04b05c3
Dominic Wroblewski <domness@gmail.com>
2013-07-02 20:43:23 +0100

    Check if remainder is present

--- lib/xero_gateway/payroll/bank_account.rb
+++ lib/xero_gateway/payroll/bank_account.rb
@@ -25,7 +25,7 @@ module XeroGateway::Payroll
         b.AccountName self.account_name if self.account_name
         b.BSB self.bsb if self.bsb
         b.AccountNumber self.account_number if self.account_number
-        b.Remainder self.remainder if self.remainder
+        b.Remainder self.remainder if !self.remainder.nil?
         b.Percentage self.percentage if self.percentage
       }
     end
