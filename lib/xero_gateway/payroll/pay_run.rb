45eb17ab2c03dad564272ebb81b6e6ce79dd7682
kemil <kemil07@gmail.com>
2013-08-08 17:18:02 +0700

    Finished GET Payrun Xero Gateway
     - Fixed combained PR
     - Finished GET All and GET by id of PayRun
     - Removed whitespaces and added EOL
    
     [Finished #52031009]

---
+++ lib/xero_gateway/payroll/pay_run.rb
@@ -0,0 +1,82 @@
+module XeroGateway::Payroll
+  class NoGatewayError < StandardError; end
+
+  class PayRun
+    include XeroGateway::Dates
+
+    GUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/ unless defined?(GUID_REGEX)
+    PAY_RUN_STATUS = ["DRAFT", "POSTED"] unless defined?(PAYRUNSTATUS)
+
+    attr_accessor :gateway
+
+    # Any errors that occurred when the #valid? method called.
+    attr_reader :errors
+
+    attr_accessor :pay_run_period_end_date, :pay_run_status, :pay_run_id, :payroll_calendar_id, :payslip_message, :payslips, :pay_run_period_start_date, :payment_date, :wages, :deductions, :tax, :super, :reimbursement, :net_pay
+
+    def initialize(params = {})
+      @errors ||= []
+
+      params = {}.merge(params)
+      params.each do |k,v|
+        self.send("#{k}=", v)
+      end
+
+      @payslips ||= []
+    end
+
+    def to_xml(b = Builder::XmlMarkup.new)
+      b.PayRun{
+        b.PayRunPeriodEndDate format_date_time(self.pay_run_period_end_date) if self.pay_run_period_end_date
+        b.PayRunStatus self.pay_run_status if self.pay_run_status
+        b.PayRunID self.pay_run_id if self.pay_run_id
+        b.PayrollCalendarID self.payroll_calendar_id if self.payroll_calendar_id
+        b.PayslipMessage self.payslip_message if self.payslip_message
+        b.Payslips{
+          self.payslips.each do |payslip|
+            payslip.to_xml(b)
+          end
+        } unless self.payslips.blank?
+        b.PayRunPeriodStartDate format_date_time(self.pay_run_period_start_date) if self.pay_run_period_start_date
+        b.PaymentDate format_date_time(self.payment_date) if self.payment_date
+        b.Wages self.wages if self.wages
+        b.Deductions self.deductions if self.deductions
+        b.Tax self.tax if self.tax
+        b.Super self.super if self.super
+        b.Reimbursement self.reimbursement	if self.reimbursement
+        b.NetPay self.net_pay if self.net_pay
+      }
+    end
+
+    def self.from_xml(pay_run_element, gateway = nil)
+      pay_run = PayRun.new
+      pay_run_element.children.each do |element|
+        case (element.name)
+          when "PayRunPeriodEndDate" then pay_run.pay_run_period_end_date = parse_date(element.text)
+          when "PayRunStatus" then pay_run.pay_run_status = element.text
+          when "PayRunID" then pay_run.pay_run_id = element.text
+          when "PayrollCalendarID" then pay_run.payroll_calendar_id = element.text
+          when "PayslipMessage" then pay_run.payslip_message = element.text
+          when "Payslips" then element.children.each{|child| pay_run.payslips << Payslip.from_xml(child, gateway)}
+          when "PayRunPeriodStartDate" then pay_run.pay_run_period_start_date = parse_date(element.text)
+          when "PaymentDate" then pay_run.payment_date = parse_date(element.text)
+          when "Wages" then pay_run.wages = element.text
+          when "Deductions" then pay_run.deductions = element.text
+          when "Tax" then pay_run.tax = element.text
+          when "Super" then pay_run.super = element.text
+          when "Reimbursement" then pay_run.reimbursement = element.text
+          when "NetPay" then pay_run.net_pay = element.text
+        end
+      end
+      pay_run
+    end
+
+    def ==(other)
+     [ :pay_run_period_end_date, :pay_run_status, :pay_run_id, :payroll_calendar_id, :payslip_message, :payslips, :pay_run_period_start_date, :payment_date, :wages, :deductions, :tax, :super, :reimbursement, :net_pay ].each do |field|
+        return false if send(field) != other.send(field)
+      end
+      return true
+    end
+
+  end
+end
---
+++ lib/xero_gateway/payroll/payroll_calendar.rb
@@ -0,0 +1,57 @@
+module XeroGateway::Payroll
+  class NoGatewayError < StandardError; end
+
+  class PayrollCalendar
+    include XeroGateway::Dates
+
+    GUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/ unless defined?(GUID_REGEX)
+    CALENDAR_TYPES =["WEEKLY", "FORTNIGHTLY", "FOURWEEKLY", "MONTHLY", "TWICEMONTHLY", "QUARTERLY"]
+
+    attr_accessor :gateway
+
+    # Any errors that occurred when the #valid? method called.
+    attr_reader :errors
+
+    attr_accessor :name, :calendar_type, :start_date, :payment_date, :payroll_calendar_id
+
+    def initialize(params = {})
+      @errors ||= []
+
+      params = {}.merge(params)
+      params.each do |k,v|
+        self.send("#{k}=", v)
+      end
+    end
+
+    def to_xml(b = Builder::XmlMarkup.new)
+      b.PayrollCalendar{
+        b.PayrollCalendarID self.payroll_calendar_id if self.payroll_calendar_id
+        b.Name self.name if self.name
+        b.CalendarType self.calendar_type if self.calendar_type
+        b.StartDate format_date_time(self.start_date) if self.start_date
+        b.PaymentDate format_date_time(self.payment_date) if self.payment_date
+      }
+    end
+
+    def self.from_xml(payroll_calendar_element, gateway = nil)
+      payroll_calendar = PayrollCalendar.new
+      payroll_calendar_element.children.each do |element|
+        case (element.name)
+          when "PayrollCalendarID" then payroll_calendar.payroll_calendar_id = element.text
+          when "Name" then payroll_calendar.name = element.text
+          when "CalendarType" then payroll_calendar.calendar_type = element.text
+          when "StartDate" then payroll_calendar.start_date = parse_date(element.text)
+          when "PaymentDate" then payroll_calendar.payment_date = parse_date(element.text)
+        end
+      end
+      payroll_calendar
+    end
+
+    def ==(other)
+     [ :name, :calendar_type, :start_date, :payment_date, :payroll_calendar_id ].each do |field|
+        return false if send(field) != other.send(field)
+      end
+      return true
+    end
+  end
+end
