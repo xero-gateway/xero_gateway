8fe6623abaeea65cf4ac44b9740dc44d4e4c87da
Nik Wakelin <me@nikwakelin.com>
2013-12-09 11:26:11 +1300

    Add Short Code to Organisation, update incorrect comments

--- lib/xero_gateway/organisation.rb
+++ lib/xero_gateway/organisation.rb
@@ -4,20 +4,21 @@ module XeroGateway
     unless defined? ATTRS
       ATTRS = {
         "Name" 	                => :string,     # Display name of organisation shown in Xero
-        "LegalName"             => :string,	   # Organisation name shown on Reports
+        "LegalName"             => :string,	    # Organisation name shown on Reports
         "PaysTax" 	            => :boolean,    # Boolean to describe if organisation is registered with a local tax authority i.e. true, false
         "Version"   	          => :string,     # See Version Types
         "BaseCurrency"          => :string,     # Default currency for organisation. See Currency types
-        "OrganisationType"      => :string,     # UNDOCUMENTED parameter, only returned for "real" (i.e non-demo) companies
-        "OrganisationStatus"    => :string,   # UNDOCUMENTED parameter
-        "IsDemoCompany"         => :boolean,    # UNDOCUMENTED parameter
-        "APIKey"                => :string,     # UNDOCUMENTED paramater, returned if organisations are linked via Xero Network
-        "CountryCode"           => :string,      # UNDOCUMENTED parameter
+        "OrganisationType"      => :string,     # only returned for "real" (i.e non-demo) companies
+        "OrganisationStatus"    => :string,     
+        "IsDemoCompany"         => :boolean,    
+        "APIKey"                => :string,     # returned if organisations are linked via Xero Network
+        "CountryCode"           => :string,      
         "TaxNumber"             => :string,
         "FinancialYearEndDay"   => :string,
         "FinancialYearEndMonth" => :string,
         "PeriodLockDate"        => :string,
-        "CreatedDateUTC"        => :string        
+        "CreatedDateUTC"        => :string,
+        "ShortCode"             => :string
       }
     end
     
