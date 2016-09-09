294a05e5450eb8ac741395033ef8cc5179c52574
Nik Wakelin <me@nikwakelin.com>
2016-09-09 22:50:18 +0100

    Swaps out alias_method_chain for Module#prepend in Ruby versions that support it

--- lib/oauth/oauth_consumer.rb
+++ lib/oauth/oauth_consumer.rb
@@ -1,14 +1,30 @@
 module OAuth
   class Consumer
-    
-    def http_with_ssl_client_certificates(*args)
-      @http ||= http_without_ssl_client_certificates(*args).tap do |http|
-        http.cert = options[:ssl_client_cert]
-        http.key  = options[:ssl_client_key]
+
+    if RUBY_VERSION >= "2.0.0"
+
+      # we got Module#prepend, let's use it
+      module ClientCertificateExtensions
+        def http
+          super.tap do |http|
+            http.cert = options[:ssl_client_cert]
+            http.key  = options[:ssl_client_key]
+          end
+        end
       end
+
+      prepend ClientCertificateExtensions
+
+    else
+      def http_with_ssl_client_certificates(*args)
+        @http ||= http_without_ssl_client_certificates(*args).tap do |http|
+          http.cert = options[:ssl_client_cert]
+          http.key  = options[:ssl_client_key]
+        end
+      end
+
+      alias_method_chain :http, :ssl_client_certificates
     end
-  
-    alias_method_chain :http, :ssl_client_certificates
-    
+
   end
 end
