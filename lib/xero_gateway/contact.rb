bbcec05dc7ff0396095c0003937e8ce1750ee793
Nik Wakelin <me@nikwakelin.com>
2016-09-27 00:07:40 +0100

    add updated at UTC to contact

--- lib/xero_gateway/contact.rb
+++ lib/xero_gateway/contact.rb
@@ -1,29 +1,29 @@
 module XeroGateway
   class Contact
     include Dates
-        
+
     GUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/ unless defined?(GUID_REGEX)
-    
+
     CONTACT_STATUS = {
       'ACTIVE' =>     'Active',
       'DELETED' =>    'Deleted'
     } unless defined?(CONTACT_STATUS)
-            
+
     # Xero::Gateway associated with this contact.
     attr_accessor :gateway
-    
+
     # Any errors that occurred when the #valid? method called.
     attr_reader :errors
-    
+
     attr_accessor :contact_id, :contact_number, :status, :name, :first_name, :last_name, :email, :addresses, :phones, :updated_at,
                   :bank_account_details, :tax_number, :accounts_receivable_tax_type, :accounts_payable_tax_type, :is_customer, :is_supplier,
                   :default_currency, :contact_groups
 
-        
+
     def initialize(params = {})
       @errors ||= []
 
-      params = {}.merge(params)      
+      params = {}.merge(params)
       params.each do |k,v|
         self.send("#{k}=", v)
       end
@@ -31,15 +31,15 @@ module XeroGateway
       @phones ||= []
       @addresses ||= []
     end
-    
+
     def address=(address)
       self.addresses = [address]
     end
-    
+
     def address
       self.addresses[0] ||= Address.new
     end
-    
+
     # Helper method to add a new address object to this contact.
     #
     # Usage:
@@ -54,11 +54,11 @@ module XeroGateway
     def add_address(address_params)
       self.addresses << Address.new(address_params)
     end
-    
+
     def phone=(phone)
       self.phones = [phone]
     end
-    
+
     def phone
       if @phones.size > 1
         @phones.detect {|p| p.phone_type == 'DEFAULT'} || phones[0]
@@ -66,7 +66,7 @@ module XeroGateway
         @phones[0] ||= Phone.new
       end
     end
-    
+
     # Helper method to add a new phone object to this contact.
     #
     # Usage:
@@ -77,41 +77,41 @@ module XeroGateway
     def add_phone(phone_params = {})
       self.phones << Phone.new(phone_params)
     end
-    
+
     # Validate the Contact record according to what will be valid by the gateway.
     #
-    # Usage: 
+    # Usage:
     #  contact.valid?     # Returns true/false
-    #  
+    #
     #  Additionally sets contact.errors array to an array of field/error.
     def valid?
       @errors = []
-      
+
       if !contact_id.nil? && contact_id !~ GUID_REGEX
         @errors << ['contact_id', 'must be blank or a valid Xero GUID']
       end
-      
+
       if status && !CONTACT_STATUS[status]
         @errors << ['status', "must be one of #{CONTACT_STATUS.keys.join('/')}"]
       end
-      
+
       unless name
         @errors << ['name', "can't be blank"]
       end
-      
+
       # Make sure all addresses are correct.
       unless addresses.all? { | address | address.valid? }
         @errors << ['addresses', 'at least one address is invalid']
       end
-      
+
       # Make sure all phone numbers are correct.
       unless phones.all? { | phone | phone.valid? }
         @errors << ['phones', 'at least one phone is invalid']
       end
-      
+
       @errors.size == 0
     end
-    
+
     # General purpose create/save method.
     # If contact_id and contact_number are nil then create, otherwise, attempt to save.
     def save
@@ -121,21 +121,21 @@ module XeroGateway
         update
       end
     end
-    
+
     # Creates this contact record (using gateway.create_contact) with the associated gateway.
     # If no gateway set, raise a NoGatewayError exception.
     def create
       raise NoGatewayError unless gateway
       gateway.create_contact(self)
     end
-    
+
     # Creates this contact record (using gateway.update_contact) with the associated gateway.
     # If no gateway set, raise a NoGatewayError exception.
     def update
       raise NoGatewayError unless gateway
       gateway.update_contact(self)
     end
-        
+
     def to_xml(b = Builder::XmlMarkup.new)
       b.Contact {
         b.ContactID self.contact_id if self.contact_id
@@ -160,7 +160,7 @@ module XeroGateway
         } if self.phones.any?
       }
     end
-    
+
     # Take a Contact element and convert it into an Contact object
     def self.from_xml(contact_element, gateway = nil)
       contact = Contact.new(:gateway => gateway)
@@ -185,11 +185,12 @@ module XeroGateway
           when "IsCustomer" then contact.is_customer = (element.text == "true")
           when "IsSupplier" then contact.is_supplier = (element.text == "true")
           when "DefaultCurrency" then contact.default_currency = element.text
+          when "UpdatedDateUTC" then contact.updated_at = parse_date_time(element.text)
         end
       end
       contact
     end
-    
+
     def ==(other)
       [ :contact_id, :contact_number, :status, :name, :first_name, :last_name, :email, :addresses, :phones, :updated_at,
         :bank_account_details, :tax_number, :accounts_receivable_tax_type, :accounts_payable_tax_type, :is_customer, :is_supplier,
@@ -197,7 +198,7 @@ module XeroGateway
         return false if send(field) != other.send(field)
       end
       return true
-    end    
-        
+    end
+
   end
 end
