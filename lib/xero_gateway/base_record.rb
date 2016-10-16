3b1380023972b6d762ad08707cc8e6f6e8de86bb
Nik Wakelin <me@nikwakelin.com>
2016-10-17 00:35:33 +0200

    Adds support for the Address fields on Organisation

--- lib/xero_gateway/base_record.rb
+++ lib/xero_gateway/base_record.rb
@@ -1,5 +1,8 @@
 module XeroGateway
   class BaseRecord
+
+    class UnsupportedAttributeType < StandardError; end
+
     class_attribute :element_name
     class_attribute :attribute_definitions
 
@@ -59,25 +62,33 @@ module XeroGateway
       if Hash === attr_definition
         element.children.each do |child|
           next unless child.respond_to?(:name)
-          
+
           child_attribute = child.name
           child_attr_definition = attr_definition[child_attribute]
+          child_attr_name       = "#{attribute}#{child_attribute}" # SalesDetails/UnitPrice => SalesDetailsUnitPrice
+
           next unless child_attr_definition
 
-          from_xml_attributes(child, "#{attribute}#{child_attribute}", child_attr_definition)
+          from_xml_attributes(child, child_attr_name, child_attr_definition)
         end
 
         return
       end
 
       value = case attr_definition
-        when :boolean then  element.text == "true"
-        when :float   then  element.text.to_f
-        when :integer then  element.text.to_i
-        else                element.text
-      end if element.text.present?
+        when :boolean  then  element.text == "true"
+        when :float    then  element.text.to_f
+        when :integer  then  element.text.to_i
+        when Array     then  array_from_xml(element, attr_definition)
+        else                 element.text
+      end if element.text.present? || element.children.present?
 
       send("#{attribute.underscore}=", value)
+    end
+
+    def array_from_xml(element, attr_definition)
+      definition_klass = attr_definition.first
+      element.children.map { |child_el| definition_klass.from_xml(child_el) }
     end
 
     def to_xml_attributes(builder = Builder::XmlMarkup.new, path = nil, attr_definitions = self.class.attribute_definitions)
@@ -86,6 +97,14 @@ module XeroGateway
         when Hash
           builder.__send__(attr) do
             to_xml_attributes(builder, "#{path}#{attr}", value)
+          end
+        when Array
+          raise UnsupportedAttributeType.new("#{value} instances don't respond to #to_xml") unless value.first.method_defined?(:to_xml)
+          value = send("#{path}#{attr}".underscore) || []
+          builder.__send__(attr) do |array_wrapper|
+            value.map do |k|
+              k.to_xml(array_wrapper)
+            end
           end
         else
           builder.__send__(attr, send("#{path}#{attr}".underscore))
