5ecfb384cc96a78de55b507cc48f9742652165d6
Steve Hoeksema <steve@kotiri.com>
2017-07-05 18:09:41 +1200

    Add Organisation ID

--- lib/xero_gateway/organisation.rb
+++ lib/xero_gateway/organisation.rb
@@ -1,6 +1,7 @@
 module XeroGateway
   class Organisation < BaseRecord
     attributes({
+      "OrganisationID"        => :string,
       "Name" 	                => :string,     # Display name of organisation shown in Xero
       "LegalName"             => :string,	    # Organisation name shown on Reports
       "PaysTax" 	            => :boolean,    # Boolean to describe if organisation is registered with a local tax authority i.e. true, false
--- test/integration/get_organisation_test.rb
+++ test/integration/get_organisation_test.rb
@@ -2,23 +2,24 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class GetOrganisationTest < Test::Unit::TestCase
   include TestHelper
-  
+
   def setup
     @gateway = XeroGateway::Gateway.new(CONSUMER_KEY, CONSUMER_SECRET)
-    
+
     if STUB_XERO_CALLS
       @gateway.xero_url = "DUMMY_URL"
-      
-      @gateway.stubs(:http_get).with {|client, url, params| url =~ /Organisation$/ }.returns(get_file_as_string("organisation.xml"))          
+
+      @gateway.stubs(:http_get).with {|client, url, params| url =~ /Organisation$/ }.returns(get_file_as_string("organisation.xml"))
     end
   end
-  
+
   def test_get_organisation
     result = @gateway.get_organisation
     assert result.success?
     assert !result.response_xml.nil?
-    
+
     assert_equal XeroGateway::Organisation, result.organisation.class
     assert_equal "Demo Company (NZ)", result.organisation.name
+    assert_equal "c3d5e782-2153-4cda-bdb4-cec791ceb90d", result.organisation.organisation_id
   end
 end
