7f601ebfa89400fe9feb1fe7eb452f184178795d
Brian Henry <brian.henry@delivered.io>
2017-09-07 15:39:56 -0500

    remove user agent as a separate param
    for consistency with existing option-based approach for required private key

--- lib/xero_gateway/partner_app.rb
+++ lib/xero_gateway/partner_app.rb
@@ -5,11 +5,13 @@ module XeroGateway
     class UserAgentRequired < StandardError; end
 
     NO_PRIVATE_KEY_ERROR_MESSAGE = "You need to provide your private key (corresponds to the public key you uploaded at api.xero.com) as :private_key_file (should be .crt or .pem files)"
+    NO_USER_AGENT_ERROR_MESSAGE = "a unique User-Agent header is required for partner apps and should be supplied as :user_agent"
 
     def_delegators :client, :session_handle, :renew_access_token, :authorization_expires_at
 
-    def initialize(consumer_key, consumer_secret, user_agent_base, options = {})
+    def initialize(consumer_key, consumer_secret, options = {})
       raise CertificateRequired.new(NO_PRIVATE_KEY_ERROR_MESSAGE) unless options[:private_key_file]
+      raise UserAgentRequired.new(NO_USER_AGENT_ERROR_MESSAGE) unless options[:user_agent]
 
       defaults = {
         :site             => "https://api.xero.com",
@@ -18,7 +20,6 @@ module XeroGateway
       }
 
       options = defaults.merge(options)
-      options[:user_agent] = user_agent_base
 
       super(consumer_key, consumer_secret, defaults.merge(options))
     end
