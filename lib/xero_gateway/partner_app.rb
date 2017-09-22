a80abdc5d54bc9ab846d55625c74351f3be32265
Brian Henry <brian.henry@delivered.io>
2017-09-22 15:59:35 -0500

    add logging of missing user agent for partner app
    note that Gateway#logger can't exist yet during subclass instantiation, so have to do direct `puts`

--- lib/xero_gateway/partner_app.rb
+++ lib/xero_gateway/partner_app.rb
@@ -2,16 +2,16 @@ module XeroGateway
   class PartnerApp < Gateway
 
     class CertificateRequired < StandardError; end
-    class UserAgentRequired < StandardError; end
 
     NO_PRIVATE_KEY_ERROR_MESSAGE = "You need to provide your private key (corresponds to the public key you uploaded at api.xero.com) as :private_key_file (should be .crt or .pem files)"
-    NO_USER_AGENT_ERROR_MESSAGE = "a unique User-Agent header is required for partner apps and should be supplied as :user_agent"
-
+    
     def_delegators :client, :session_handle, :renew_access_token, :authorization_expires_at
 
     def initialize(consumer_key, consumer_secret, options = {})
       raise CertificateRequired.new(NO_PRIVATE_KEY_ERROR_MESSAGE) unless options[:private_key_file]
-      raise UserAgentRequired.new(NO_USER_AGENT_ERROR_MESSAGE) unless options[:user_agent]
+      
+      #required by Xero for new partner apps, but only issuing warning to keep backward compat for any grandfathered apps
+      puts "WARNING: a unique User-Agent header is required for Xero partner apps, and is missing - this should be supplied as :user_agent" unless options[:user_agent]
 
       defaults = {
         :site             => "https://api.xero.com",
