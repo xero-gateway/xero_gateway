ed4fdc32984173ed36f87d6c7b25b9684e90bbc8
Dat Le <dat.le@employmenthero.com>
2021-04-20 16:23:44 +0700

    Change branch development to staging
    (cherry picked from commit 8c33063b7fe58295c7a4974ab60a29b0dc8cd1af)

--- scripts/publish_staging.rb
+++ scripts/publish_staging.rb
@@ -7,6 +7,6 @@ package_name = app['name']
 
 gemspec_file_name = Dir.glob('./*.gemspec')[0].split('/')[-1]
 
-abort('Development Gem must have .dev in version') unless version.include?('dev')
+abort('Pre-release Gem must have .alpha in version') unless version.include?('alpha')
 
 exec("gem build #{gemspec_file_name} && curl -F package=@#{package_name}-#{version}.gem https://#{ENV['GEMFURY_TOKEN']}@push.fury.io/#{ENV['GEMFURY_PACKAGE']}/")
