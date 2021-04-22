32541f7336587a5b9e85d65059003fc6028f2069
Dat Le <dat.le@employmenthero.com>
2021-04-22 10:22:03 +0700

    Auto versioning staging publish

--- scripts/publish_staging.rb
+++ scripts/publish_staging.rb
@@ -2,11 +2,17 @@ require 'json'
 
 manifest_file = File.read(File.expand_path('../../app.json', __FILE__))
 app = JSON.parse(manifest_file)
-version = app['version']
+
+commit = ARGV[0]
+version = "#{app['version']}.pre.#{commit[0..7]}"
 package_name = app['name']
 
-gemspec_file_name = Dir.glob('./*.gemspec')[0].split('/')[-1]
+# Update back manifest file
+app['version'] = version
+File.open(manifest_file_path, "w") do |f|
+  f.write(JSON.pretty_generate(app))
+end
 
-abort('Pre-release Gem must have .alpha in version') unless version.include?('alpha')
+gemspec_file_name = Dir.glob('./*.gemspec')[0].split('/')[-1]
 
 exec("gem build #{gemspec_file_name} && curl -F package=@#{package_name}-#{version}.gem https://#{ENV['GEMFURY_TOKEN']}@push.fury.io/#{ENV['GEMFURY_PACKAGE']}/")
